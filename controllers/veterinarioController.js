5bcab4ac5a2172eabfcaae6fbc2be133a23270a5
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 13:05:48 -0600

    Función comprobarPassword agregada en Veterinario.js para comprobar el password del usuario con el hasheado en la BD. Revisión de password agregada en la función autenticar() del veterinarioController.js

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -46,7 +46,7 @@ const confirmar = async (req, res) => {
 };
 
 const autenticar = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   // Comprobar si el usuario existe
   const usuario = await Veterinario.findOne({ email });
@@ -58,6 +58,14 @@ const autenticar = async (req, res) => {
   // Comprobar si el usuario está confirmado o no
   if (!usuario.confirmado) {
     const error = new Error('Tu Cuenta no ha sido confirmada');
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Revisar password
+  if (await usuario.comprobarPassword(password)) {
+    // Autenticar
+  } else {
+    const error = new Error('El password es incorrecto');
     return res.status(403).json({ msg: error.message });
   }
 };
