48f65b199a86363987431ae3866042da502541df
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 13:44:04 -0600

    Rutas para reestablecimiento de contraseña agregadas en veterinarioRoutes.js. Funciones para las rutas correspondientes agregadas en veterinarioController.js

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -4,15 +4,21 @@ import {
   registrar,
   confirmar,
   autenticar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
 } from '../controllers/veterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+// Área pública
 router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
-
+router.post('/olvide-password', olvidePassword);
+router.route('olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+// Área privada
 router.get('/perfil', checkAuth, perfil);
 
 export default router;
