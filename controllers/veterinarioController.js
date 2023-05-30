024ec441c5e5968955dbe54fa3a6ac93b2537ef8
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 12:03:59 -0600

    Nueva ruta dinámica para confirmar Token agregada. Se añadió la función confirmar() en veterinarioController.js

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -25,4 +25,10 @@ const registrar = async (req, res) => {
 const perfil = (req, res) => {
   res.json({ msg: 'Mostrando perfil' });
 };
-export { registrar, perfil };
+
+const confirmar = (req, res) => {
+  console.log(req.params.token);
+  res.json({ msg: 'Confirmando cuenta...' });
+};
+
+export { registrar, perfil, confirmar };
