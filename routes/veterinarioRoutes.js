acd8b21dab95c83b11c818bed647d5114feb6fb5
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 12:50:37 -0600

    Ruta de login para veterinarios agregada junto con la función de su controlador correspondiente

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -3,11 +3,13 @@ import {
   perfil,
   registrar,
   confirmar,
+  autenticar,
 } from '../controllers/veterinarioController.js';
 const router = express.Router();
 
 router.post('/', registrar);
 router.get('/perfil', perfil);
 router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
 
 export default router;
