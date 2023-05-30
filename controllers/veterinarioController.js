b5129573111e851356f05432b1686d8bc2834a73
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 11:19:08 -0600

    Las solicitudas se configuraron a tipo JSON en index.js. Extracción de nombre, password y email en la función registrar() en el veterinarioController.js

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,4 +1,11 @@
 const registrar = (req, res) => {
+  console.log(req.body);
+  const { nombre, email, password } = req.body;
+
+  console.log(nombre);
+  console.log(email);
+  console.log(password);
+
   res.json({ msg: 'Registrando usuario' });
 };
 
