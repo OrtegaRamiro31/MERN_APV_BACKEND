b259ecbeb62213bfab00177e06fea6f89c562f2c
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 11:13:05 -0600

    Respuestas de funciones en veterinarioController.js cambiadas a json

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { perfil, registrar } from '../controllers/veterinarioController.js';
 const router = express.Router();
 
-router.get('/', registrar);
+router.post('/', registrar);
 
 router.get('/perfil', perfil);
 
