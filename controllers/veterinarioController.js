170cd1f44a11d4d823cfe4f664a7b3057204e24a
ramiro <ceinnova13458@innovaccion.mx>
2023-05-31 13:26:49 -0600

    Estructura de código para enviar correo para reestablecer contraseña agregado en emailOlvidePassword.js. La función emailOlvidePassword() del archivo anterior se instancia en la función olvidePassword en veterinarioController.js

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -2,6 +2,7 @@ import Veterinario from '../models/Veterinario.js';
 import generarJWT from '../helpers/generarJWT.js';
 import generarId from '../helpers/generarId.js';
 import emailRegistro from '../helpers/emailRegistro.js';
+import emailOlvidePassword from '../helpers/emailOlvidePassword.js';
 
 const registrar = async (req, res) => {
   const { email, nombre } = req.body;
@@ -94,6 +95,13 @@ const olvidePassword = async (req, res) => {
   try {
     existeVeterinario.token = generarId();
     await existeVeterinario.save();
+
+    // Enviar Email con instrucciones
+    emailOlvidePassword({
+      email,
+      nombre: existeVeterinario.nombre,
+      token: existeVeterinario.token,
+    });
     res.json({ msg: 'Hemos enviado un email con las instrucciones' });
   } catch (error) {
     console.log(error);
